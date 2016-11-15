802b154d7289224270fe745e3a0adbf8daf226ce
goldy1992 <goldy13_1992@hotmail.co.uk>
2016-11-15 15:43:00 +0100

    removed target files

--- practica2/target/classes/create-table.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE Usuario (
-  id         INTEGER PRIMARY KEY,
-  nombre VARCHAR(30),
-  apellido VARCHAR(30),
-  email  VARCHAR(50)
-);
--- practica2/target/classes/insert-data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO Usuario VALUES (1, 'Joe', 'Bloggs', 'jb@gmail.com');
-INSERT INTO Usuario VALUES (2, 'alex', 'Kolorov', 'alex@yahoo.com');
-INSERT INTO Usuario VALUES (3, 'joel', 'Hart', 'joel@gmail.com');
